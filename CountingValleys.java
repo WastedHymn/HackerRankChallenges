51c8925d757adbe2ab35dae305c61b50fbdf32bc
WastedHymn <49368530+WastedHymn@users.noreply.github.com>
2019-07-31 14:08:32 +0300

    Create CountingValleys.java

---
+++ CountingValleys.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the countingValleys function below.
+    static int countingValleys(int n, String s) {
+        int altitude = 0, counter = 0;
+        char character = 'a';
+        char[] string = s.toCharArray();
+        for(int i=0; i<string.length; i++){
+            character = string[i];
+            if(character=='D'){
+                if(altitude == 0){
+                    counter++;
+                }
+                altitude--;
+            }else if(character=='U'){
+                altitude++;
+            }
+        }
+        return counter;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        String s = scanner.nextLine();
+
+        int result = countingValleys(n, s);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
